2663af05d0835ab7dd3802cbaaa688228efc470c
Andy Britt <AndyB.WebDev@protonmail.com>
2022-03-29 21:38:26 -0400

    fixed Finally

--- week-2/Britt-payroll.js
+++ week-2/Britt-payroll.js
@@ -4,41 +4,44 @@
 ; 2022
 */
 
-let firstName = "Bob"
-let lastName = "Burger"
-let address = "123 A Street"
-let hireDate = "12/34/56"
-let payRate =  30.783449999999
-let payRateFixed = payRate.toFixed(1)
+const firstName = "Robert"
+const lastName = "Burgers"
+const address = "123 A Street"
+const hireDate = new Date();
+const hireDateFixed = hireDate.toLocaleDateString();
+const payRate =  30.783449999999
+const payRateFixed = payRate.toFixed(1)
 
 document.getElementById("txtFirstName").innerHTML = firstName; 
 document.getElementById("txtLastName").innerHTML = lastName;
 document.getElementById("txtAddress").innerHTML = address;
-document.getElementById("txtHireDate").innerHTML = hireDate;
+document.getElementById("txtHireDate").innerHTML = hireDateFixed;
 document.getElementById("txtPayRate").innerHTML = payRateFixed;
 
-let firstNameTwo = "Rick"
-let lastNameTwo = "Sanchez"
-let addressTwo = "456 B Street"
-let hireDateTwo = "01/01/75"
-let payRateTwo = 28.138999
-let payRateTwoFixed = payRateTwo.toFixed(1)
+const firstNameTwo = "Richard"
+const lastNameTwo = "Sanchez"
+const addressTwo = "456 B Street"
+const hireDateTwo = new Date()
+const hireDateTwoFixed = hireDateTwo.toLocaleDateString();
+const payRateTwo = 28.138999
+const payRateTwoFixed = payRateTwo.toFixed(1)
 
 document.getElementById("txtFirstNameTwo").innerHTML = firstNameTwo;
 document.getElementById("txtLastNameTwo").innerHTML = lastNameTwo;
 document.getElementById("txtAddressTwo").innerHTML = addressTwo;
-document.getElementById("txtHireDateTwo").innerHTML = hireDateTwo;
+document.getElementById("txtHireDateTwo").innerHTML = hireDateTwoFixed;
 document.getElementById("txtPayRateTwo").innerHTML = payRateTwoFixed;
 
-let firstNameThree = "Albert"
-let lastNameThree = "Einstein"
-let addressThree = "789 C Street"
-let hireDateThree = "02/10/25"
-let payRateThree = 3.149999
-let payRateThreeFixed = payRateThree.toFixed(1)
+const firstNameThree = "Alberta"
+const lastNameThree = "Einstein"
+const addressThree = "789 C Street"
+const hireDateThree = new Date()
+const hireDateThreeFixed = hireDateThree.toLocaleDateString();
+const payRateThree = 3.149999
+const payRateThreeFixed = payRateThree.toFixed(1)
 
 document.getElementById("txtFirstNameThree").innerHTML = firstNameThree;
 document.getElementById("txtLastNameThree").innerHTML = lastNameThree;
 document.getElementById("txtAddressThree").innerHTML = addressThree;
-document.getElementById("txtHireDateThree").innerHTML = hireDateThree;
+document.getElementById("txtHireDateThree").innerHTML = hireDateThreeFixed;
 document.getElementById("txtPayRateThree").innerHTML = payRateThreeFixed;

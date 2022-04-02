58dff71709683f74618fc4bd13103be5f323ab05
Andy Britt <AndyB.WebDev@protonmail.com>
2022-04-02 11:06:48 -0400

    Final draft of code for Temp app

--- week-3/Britt-temp-conversion.js
+++ week-3/Britt-temp-conversion.js
@@ -1,14 +1,15 @@
 /*
-=================================
+===========================================
 ; File: Britt-temp-conversion.js
 ; Author: Andrew Britt
 ; Date: 1 Apr 2022
 ; Description: JavaScript code to 
 ; convert fahrenheit to celsius.
-;
-;
-=================================
+; Reference: https://www.w3schools.com/howto/howto_js_temperature_converter.asp
+; Date reference accessed: 1 Apr 2022
+===========================================
 */
+
 // The function tToC 
 function tToC(conversionVal) {
     conversionVal = ((parseFloat(conversionVal) - 32) / 1.8).toFixed(2);

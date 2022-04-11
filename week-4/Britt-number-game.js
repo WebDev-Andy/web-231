782cdd1f59fba1259f92c40955461620b3ca257f
Andy Britt <AndyB.WebDev@protonmail.com>
2022-04-10 21:12:24 -0400

    Final draft for number game

--- week-4/Britt-number-game.js
+++ week-4/Britt-number-game.js
@@ -3,9 +3,9 @@
 ; File: Britt-number-game.js
 ; Author: Andrew Britt
 ; Date: 4 Apr 2022
-; Description: JavaScript code
-; Reference: 
-; Date reference accessed: 
+; Description: JavaScript code for assignment 4
+; Reference: https://www.youtube.com/watch?v=gPbpGWFl8mQ&t=622s
+; Date reference accessed: 6 April 2022
 ===========================================
 */
 
@@ -13,19 +13,19 @@ let output = document.getElementById("results");
 let btn = document.getElementById("btnSubmit");
 let myNumber = document.getElementById("txtMyNumber");
 const rnd = Math.floor((Math.random() * 10) + 1);
-// console.log(rnd);    <-- Code to show the random number in console
+// console.log(rnd);    <-- Code to show the random number in console to check that it is working.
 
 function guessNumber() {
     let myNumber = document.getElementById("txtMyNumber").value;
 
     if(myNumber == rnd) {
-        output.innerHTML="Way to go, you're smarter than most animals!";
+        output.innerHTML="Congratulations! You picked the correct number!";
     }
     else if(myNumber > rnd) {
-        output.innerHTML = "Nope, try lower";
+        output.innerHTML = "The number is less than " + myNumber;
     }
     else if(myNumber < rnd) {
-        output.innerHTML = "Nope, go higher"
+        output.innerHTML = "The number is greater than " + myNumber;
     }
 
 
